8a04411d74b87d329f6b6672cba0667e63862f84
Natan <natanparker@gmail.com>
2019-04-04 13:57:25 -0400

    hehe

--- Testing.java
+++ Testing.java
@@ -11,6 +11,6 @@ package FST;
 
 public class Testing {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("DAvid is gay");
     }
 }
