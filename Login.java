f008aa86d04441ee5b7884d9005db2bf1d876ce9
Natan <natanparker@gmail.com>
2019-04-16 21:46:20 -0400

    SWitched to Java 12, may still work with 11.

--- Login.java
+++ Login.java
@@ -28,7 +28,7 @@ public class Login extends Application {
 	}
 
 	@Override
-	public void start (Stage primaryStage) throws Exception {
+	public void start (Stage primaryStage) {
 		primaryStage.setTitle("De Bank");
 		primaryStage.show();
 
